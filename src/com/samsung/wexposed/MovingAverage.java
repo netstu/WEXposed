d88686f60d89d339c6699e59890b867729edc271
Bo Tang <botang.usa@gmail.com>
2015-02-19 15:27:13 -0800

    MovingAverage algorithm improved

--- src/com/samsung/wexposed/MovingAverage.java
+++ src/com/samsung/wexposed/MovingAverage.java
@@ -4,38 +4,48 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class MovingAverage {
-	private int window_size;
-	private double cur_avg;
-	private Queue<Double> win;
+    private double curAvg;
+    private int curIndex;
+    private long[] window;
+    private boolean full;
 
-	public MovingAverage(int ws) {
-		if (ws <= 0)
-			return;
-		window_size = ws;
-		cur_avg = 0;
-		win = new LinkedList<Double>();
-	}
+    public MovingAverage(int ws) {
+        if (ws <= 1) return;    //the window size cannot be smaller than 2
+        curAvg = 0;
+        window = new long[ws];
+        for(int i=0; i<ws; i++) window[i] = 0;    //initialize the array with all zeros
+        curIndex = 0;
+        full = false;
+    }
 
-	public double next(double num) {
-		double tmp = 0;
-		int w1 = win.size();
-		boolean full = false;
+    public double next(long num) {
+        int w1 = curIndex;        // curIndex is equal to current window size if not full
+        if(full) w1 = window.length;
+        
+        long tmp = add(num);
+        
+         
+        
+        int w2 = curIndex;
+        if(full) w2 = window.length;
 
-		while (win.size() >= window_size) {
-			tmp = win.poll();
-			full = true;
-		}
+//       System.out.println("tmp = " + tmp + ", num = " + num + ", w1 = " + w1 + ", w2 = " + w2 + ", full = " + full  + ", curIndex = " + curIndex);
+        curAvg = curAvg * w1 / w2 + (num - tmp) / w2;
 
-		win.add(num);
-		// int w2 = win.size();
-		// System.out.print("tmp = " + tmp + ", num = " + num + ", w1 = " + w1 +
-		// ", w2 = " + w2 + "\t");
-
-		if (!full)
-			cur_avg = cur_avg * w1 / win.size();
-		cur_avg += (num - tmp) / win.size();
-
-		// System.out.println("post_cur_avg = " + cur_avg);
-		return cur_avg;
-	}
+        
+//        System.out.println("post_cur_avg = " + cur_avg);
+        return curAvg;
+    }
+    
+    //@param num: the number to be added to the current window
+    //@return: if the window is full, the least recently added number will be returned; otherwise 0.
+    private long add(long num){
+        long ret = window[curIndex];
+        window[curIndex++] = num;
+        if(curIndex == window.length){
+            full = true;
+            curIndex = 0;
+        }        
+        return ret;
+    }
 }
